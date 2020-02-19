4a41d68e3130fc7b1becf5423170fbc0078b91e8
Yingqi Chen <chanwk18@hotmail.com>
2020-02-19 20:45:51 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,20 @@
 // Code SimpleComponent Here
+// Code SimpleComponent Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component{
+
+    state={
+        mood: "happy"
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.mood}
+            </div>
+
+        )
+    }
+}
+
